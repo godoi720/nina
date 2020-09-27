0301039b6cc398bcbb91e639fa2baa1eacf33896
karen godoi <karenalenice@gmail.com>
2020-09-27 12:42:54 -0300

    adicionado projeto

---
+++ 12345/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _12345
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Uma escola te contratou para desenvolver um aplicativo que realize a média das notas de alunos de uma sala,
+            // lembrando que atualmente a sala tem apenas 5 alunos e a nota vai de 0 a 10. 
+
+            Console.WriteLine("Media de alunos");
+
+            float soma = 0;
+            float media = 0;
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Digite a nota do aluno " + i + ";");
+                float nota = 0;
+                float.TryParse(Console.ReadLine(), out nota);
+                soma += nota;
+
+            }
+
+            media = soma / 5;
+            Console.WriteLine("Média é igual a: " + media);
+
+        }
+    }
+}
